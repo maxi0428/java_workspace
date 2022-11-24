3977b30940fc7f3abae39eb39b8b6ebc350d320d
maxi0428 <happy.bume@gmail.com>
2022-11-24 11:42:30 +0900

    221124 java 수업

---
+++ src/ch03_test_1018/Hello.java
@@ -0,0 +1,20 @@
+package ch03_test_1018;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 실행은 ctrl + f11
+		// 주석이 보통은 ctrl + / : 단일행 주석
+		// 여러행 주석 : ctrl + shift + /
+		// 여러행 주석 해제 : ctrl + shift + \
+		// 자동 포맷 정렬 : ctrl + shift + f
+
+		System.out.println("Hello 임근범 자바");
+		System.out.println("Hello 임근범 자바");
+		System.out.println("Hello 임근범 자바");
+ 
+		System.out.println("Hello 임근범 자바");
+	}
+
+}
